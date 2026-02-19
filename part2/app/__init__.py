9f10f77e869a02a568be1842becb934c98748673
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-02-19 17:02:19 +0300

    Refactor API initialization in __init__.py and remove unused bytecode files

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,24 +1,12 @@
 from flask import Flask, redirect, url_for
 from flask_restx import Api
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
 
 def create_app():
     app = Flask(__name__)
-
-    api = Api(
-        app,
-        version='1.0',
-        title='HBnB API',
-        description='HBnB Application API',
-        doc='/api/v1/'
-    )
-    
-    from app.api.v1.amenities import api as amenities_ns
-    from app.api.v1.places import api as places_ns
-    from app.api.v1.reviews import api as reviews_ns
-
-    @app.route('/api/v1/')
-    def redirect_to_api():
-        return redirect('/api/v1/')
+    api= Api(app)
 
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
