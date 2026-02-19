3d0a891725994ce992d0d6f92714eb33ee043edf
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-02-19 09:48:43 +0300

    add a Validations for max length 50

--- part2/models/amenity.py
+++ part2/models/amenity.py
@@ -1,9 +1,16 @@
 from models.base_model import BaseModel
 
-
 class Amenity(BaseModel):
-    def __init__(self, name):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+#-----------------------------------
+        # Validations
+        name = kwargs.get("name")
         if not name:
             raise ValueError("Amenity name cannot be empty")
         self.name = name
+
+        # Max length 50
+        if len(self.name) > 50:
+            raise ValueError("Amenity name cannot exceed 50 characters")
