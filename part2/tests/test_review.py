65601dcb0ad2c8cff15068824f30744608bef9df
MohammedError <mohammed.secure@outlook.sa>
2026-02-22 05:00:22 +0300

    Add unit tests for Review entity

---
+++ part2/tests/test_review.py
@@ -0,0 +1,16 @@
+import unittest
+from app import create_app
+
+class TestReviewEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_review_invalid(self):
+        response = self.client.post("/api/v1/reviews/", json={
+            "text": "",
+            "user_id": "fake",
+            "place_id": "fake"
+        })
+        self.assertEqual(response.status_code, 400)
